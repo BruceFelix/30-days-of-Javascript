623e0c13135a4a640f251bb0de9bc136c4c200dd
Bruce Felix <brucefelixm@gmail.com>
2022-09-06 20:21:03 +0300

    Working on mini project

---
+++ Exercises/Day21/Mini Project 1/miniproject.js
@@ -0,0 +1,32 @@
+let uls = document.querySelectorAll('li')
+uls.forEach((li, i) =>{
+    li.style.listStyle = "none"
+    li.style.margin = "1px"
+    li.style.padding = "10px 20px"
+    li.style.color = "#222"
+    if(i == 0){
+        li.style.backgroundColor = "green"
+    }
+    else if(i == 1){
+        li.style.backgroundColor = "yellow"
+    }
+    else{
+        li.style.backgroundColor = "red"
+    }
+}
+git 
+)
+let content = document.querySelector('.wrapper')
+content.style.width = "600px"
+content.style.margin = "60px auto"
+let body = document.querySelector('body')
+body.style.display = "flex column"
+
+let h1 = document.querySelector('h1')
+let h2 = document.querySelector('h2')
+h1.style.textAlign = "center"
+h2.style.textAlign = "center"
+h1.textContent = "Asabeneh Yetayeh challenges in <span id = 'year'>2020<span>"
+document.querySelector('#year').fontSize = '50px' 
+
+
--- Exercises/Day21/day21.js
+++ Exercises/Day21/day21.js
@@ -19,3 +19,17 @@ allPs[0].classList.add("oNe")
 allPs[1].id =" TWO"
 allPs[2].className = "Three"
 allPs[2].classList.add("THREE")
+
+allPs.forEach((paragraphs, i) => {
+    paragraphs.style.backgroundColor = "yellow"
+    paragraphs.style.fontSize = "24px"
+    paragraphs.style.border = "5px solid green"
+    paragraphs.style.color = "Grey"
+    if(i % 2 == 0){
+        paragraphs.style.color = "red"
+    }
+    else{
+    paragraphs.style.color = "green"
+    }
+}
+)
